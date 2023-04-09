7314a5c758033977b9d5216ca37c3d548ca6089e
4Phoenix <josephdej.caro44@gmail.com>
2023-04-09 12:47:11 -0500

    Adelanto domingo

--- components_detector.py
+++ components_detector.py
@@ -1,5 +1,21 @@
 import cv2
 import numpy as np
+import math as mt
 from skimage import io
 from skimage.filters import threshold_otsu
 import skimage
+import matplotlib.pyplot as plt
+
+#obtenemos la imagen original y la pasamos a grises
+img_org = cv2.imread("images\org.jpg")
+img_gray = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
+
+#hacemos un blurr
+img_brd = cv2.GaussianBlur(img_gray, (5, 5), 0)
+
+#hacemos un threshold
+
+
+plt.imshow(img_brd, cmap='gray')
+plt.title("Original")
+plt.show()
