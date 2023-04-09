681b0b9271f55eb6c6c2860c74eed8a7673c0313
4Phoenix <josephdej.caro44@gmail.com>
2023-04-09 15:20:19 -0500

    Detectando marcas

--- components_detector.py
+++ components_detector.py
@@ -6,16 +6,68 @@ from skimage.filters import threshold_otsu
 import skimage
 import matplotlib.pyplot as plt
 
+#código para cambiar el tamaño de la imagen
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
 #obtenemos la imagen original y la pasamos a grises
 img_org = cv2.imread("images\org.jpg")
-img_gray = cv2.cvtColor(img_org, cv2.COLOR_BGR2GRAY)
+img_re = ResizeWithAspectRatio(img_org, width=480)
+img_gray = cv2.cvtColor(img_re, cv2.COLOR_BGR2GRAY)
 
 #hacemos un blurr
 img_brd = cv2.GaussianBlur(img_gray, (5, 5), 0)
 
 #hacemos un threshold
+(T, threshImg) = cv2.threshold(img_brd, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
+#Aplicamos componentes conectados
+conn = 4
+output = cv2.connectedComponentsWithStats(threshImg, conn, cv2.CV_32S)
+(numLabels, labels, stats, centroids) = output
 
-plt.imshow(img_brd, cmap='gray')
-plt.title("Original")
-plt.show()
+for i in range(0, numLabels):
+	if i == 0:
+		text = "examining component {}/{} (background)".format(
+			i + 1, numLabels)
+	else:
+		text = "examining component {}/{}".format( i + 1, numLabels)
+	print("[INFO] {}".format(text))
+	x = stats[i, cv2.CC_STAT_LEFT]
+	y = stats[i, cv2.CC_STAT_TOP]
+	w = stats[i, cv2.CC_STAT_WIDTH]
+	h = stats[i, cv2.CC_STAT_HEIGHT]
+	area = stats[i, cv2.CC_STAT_AREA]
+	(cX, cY) = centroids[i]
+
+for i in range(0, numLabels):
+
+  # extract the connected component statistics and centroid for
+  # the current label
+  x = stats[i, cv2.CC_STAT_LEFT]
+  y = stats[i, cv2.CC_STAT_TOP]
+  w = stats[i, cv2.CC_STAT_WIDTH]
+  h = stats[i, cv2.CC_STAT_HEIGHT]
+  area = stats[i, cv2.CC_STAT_AREA]
+  (cX, cY) = centroids[i]
+
+  output = img_re.copy()
+  cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 3)
+  cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
+
+  print("Label No {}".format(i))
+  cv2.imshow(" ",output)
+  cv2.waitKey(0)  
+  cv2.destroyAllWindows()
